f19c28570867651c03bb6df82d6f5c955197b2d2
Geovanny <geovanny19991@gmail.com>
2021-05-14 17:56:50 +0200

    Kezdő állapot előállítása

---
+++ iet2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VDS.RDF;
+using VDS.RDF.Writing;
+
+namespace iet2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IGraph g = new Graph();
+
+            IUriNode dotNetRDF = g.CreateUriNode(UriFactory.Create("http://www.dotnetrdf.org"));
+            IUriNode says = g.CreateUriNode(UriFactory.Create("http://example.org/says"));
+            ILiteralNode helloWorld = g.CreateLiteralNode("Hello World");
+            ILiteralNode bonjourMonde = g.CreateLiteralNode("Bonjour tout le Monde", "fr");
+
+            g.Assert(new Triple(dotNetRDF, says, helloWorld));
+            g.Assert(new Triple(dotNetRDF, says, bonjourMonde));
+
+            foreach (Triple t in g.Triples)
+            {
+                Console.WriteLine(t.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
