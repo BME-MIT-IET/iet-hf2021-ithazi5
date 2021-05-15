fa03b5ef34d441f56bdc02682a4254862978bc76
Rittgasszer Akos <akos.rittgasszer@gmail.com>
2021-05-15 15:24:30 +0200

    Manual testing enviroment

---
+++ iet2/ManualTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VDS.RDF;
+using VDS.RDF.Parsing;
+
+namespace iet2
+{
+    class ManualTest
+    {
+        private readonly int numOfTests = 1;
+        private int numOfSucces = 0;
+
+        private IGraph g = new Graph();
+
+        public void runAllManualTests()
+        {
+            Console.WriteLine("Manual tests: ");
+            if (test1())
+                ++numOfSucces;
+            else
+                Console.WriteLine("test1 was not succesful!");
+            Console.WriteLine("{0}/{1} test was succesfull.",numOfSucces, numOfTests);
+        }
+
+        private bool test1()
+        {
+            Notation3Parser n3parser = new Notation3Parser();
+            try
+            {
+                //Load using Filename
+                n3parser.Load(g, "szepmuveszeti.n3");
+            }
+            catch (RdfParseException parseEx)
+            {
+                //This indicates a parser error e.g unexpected character, premature end of input, invalid syntax etc.
+                Console.WriteLine("Parser Error");
+                Console.WriteLine(parseEx.Message);
+                return false;
+            }
+            catch (RdfException rdfEx)
+            {
+                //This represents a RDF error e.g. illegal triple for the given syntax, undefined namespace
+                Console.WriteLine("RDF Error");
+                Console.WriteLine(rdfEx.Message);
+                return false;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+
+
+
+
+
+            int i = 0;
+            //foreach (Triple t in g.Triples)
+            //{
+            //    Console.WriteLine(t.ToString());
+            //    i++;
+            //    if (i == 100)
+            //        break;
+            //}
+            return true;
+        }
+
+    }
+}
--- iet2/Program.cs
+++ iet2/Program.cs
@@ -14,59 +14,62 @@ namespace iet2
         static void Main(string[] args)
         {
 
-            IGraph g = new Graph();
+            ManualTest manualTest = new ManualTest();
+            manualTest.runAllManualTests();
 
-            IUriNode dotNetRDF = g.CreateUriNode(UriFactory.Create("http://www.dotnetrdf.org"));
-            IUriNode says = g.CreateUriNode(UriFactory.Create("http://example.org/says"));
-            ILiteralNode helloWorld = g.CreateLiteralNode("Hello World");
-            ILiteralNode bonjourMonde = g.CreateLiteralNode("Bonjour tout le Monde", "fr");
+        //    IGraph g = new Graph();
 
-            g.Assert(new Triple(dotNetRDF, says, helloWorld));
-            g.Assert(new Triple(dotNetRDF, says, bonjourMonde));
+        //    IUriNode dotNetRDF = g.CreateUriNode(UriFactory.Create("http://www.dotnetrdf.org"));
+        //    IUriNode says = g.CreateUriNode(UriFactory.Create("http://example.org/says"));
+        //    ILiteralNode helloWorld = g.CreateLiteralNode("Hello World");
+        //    ILiteralNode bonjourMonde = g.CreateLiteralNode("Bonjour tout le Monde", "fr");
+
+        //    g.Assert(new Triple(dotNetRDF, says, helloWorld));
+        //    g.Assert(new Triple(dotNetRDF, says, bonjourMonde));
 
             
 
-            Notation3Parser n3parser = new Notation3Parser();
-            try
-            {
-                //Load using Filename
-                n3parser.Load(g, "szepmuveszeti.n3");
-            }
-            catch (RdfParseException parseEx)
-            {
-                //This indicates a parser error e.g unexpected character, premature end of input, invalid syntax etc.
-                Console.WriteLine("Parser Error");
-                Console.WriteLine(parseEx.Message);
-            }
-            catch (RdfException rdfEx)
-            {
-                //This represents a RDF error e.g. illegal triple for the given syntax, undefined namespace
-                Console.WriteLine("RDF Error");
-                Console.WriteLine(rdfEx.Message);
-            }
+        //    Notation3Parser n3parser = new Notation3Parser();
+        //    try
+        //    {
+        //        //Load using Filename
+        //        n3parser.Load(g, "szepmuveszeti.n3");
+        //    }
+        //    catch (RdfParseException parseEx)
+        //    {
+        //        //This indicates a parser error e.g unexpected character, premature end of input, invalid syntax etc.
+        //        Console.WriteLine("Parser Error");
+        //        Console.WriteLine(parseEx.Message);
+        //    }
+        //    catch (RdfException rdfEx)
+        //    {
+        //        //This represents a RDF error e.g. illegal triple for the given syntax, undefined namespace
+        //        Console.WriteLine("RDF Error");
+        //        Console.WriteLine(rdfEx.Message);
+        //    }
 
-            IBlankNode b = g.GetBlankNode("nodeID");
-            if (b != null)
-            {
-                Console.WriteLine("Blank Node with ID " + b.InternalID + " exists in the Graph");
-            }
-            else
-            {
-                Console.WriteLine("No Blank Node with the given ID existed in the Graph");
-            }
+        //    IBlankNode b = g.GetBlankNode("nodeID");
+        //    if (b != null)
+        //    {
+        //        Console.WriteLine("Blank Node with ID " + b.InternalID + " exists in the Graph");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("No Blank Node with the given ID existed in the Graph");
+        //    }
 
 
 
-            int i = 0;
-            foreach (Triple t in g.Triples)
-            {
-                Console.WriteLine(t.ToString());
-                i++;
-                if (i == 100)
-                    break;
-            }
+        //    int i = 0;
+        //    foreach (Triple t in g.Triples)
+        //    {
+        //        Console.WriteLine(t.ToString());
+        //        i++;
+        //        if (i == 100)
+        //            break;
+        //    }
 
-            Console.ReadKey();
+        //    Console.ReadKey();
         }
     }
 }
