703da2125340f959d7d9b5ed550188c3bd7874b2
Geovanny27 <geovanny19991@gmail.com>
2021-05-15 13:44:48 +0200

    Teljesítménytesztelések megírása

--- iet2/Program.cs
+++ iet2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using VDS.RDF;
@@ -7,24 +8,19 @@ using VDS.RDF.Parsing;
 using VDS.RDF.Query;
 using VDS.RDF.Writing;
 
+
+
 namespace iet2
 {
+    
     class Program
     {
-        static void Main(string[] args)
+        private static void PerformanceTestWithDatabase()
         {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
 
             IGraph g = new Graph();
-
-            IUriNode dotNetRDF = g.CreateUriNode(UriFactory.Create("http://www.dotnetrdf.org"));
-            IUriNode says = g.CreateUriNode(UriFactory.Create("http://example.org/says"));
-            ILiteralNode helloWorld = g.CreateLiteralNode("Hello World");
-            ILiteralNode bonjourMonde = g.CreateLiteralNode("Bonjour tout le Monde", "fr");
-
-            g.Assert(new Triple(dotNetRDF, says, helloWorld));
-            g.Assert(new Triple(dotNetRDF, says, bonjourMonde));
-
-            
 
             Notation3Parser n3parser = new Notation3Parser();
             try
@@ -55,16 +51,133 @@ namespace iet2
                 Console.WriteLine("No Blank Node with the given ID existed in the Graph");
             }
 
+            sw.Stop();
+            Console.WriteLine("Load time: " + sw.ElapsedMilliseconds + " ms");
+        }
 
+        private static void PerformanceTestWith1Triple()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
 
-            int i = 0;
-            foreach (Triple t in g.Triples)
+            IGraph g = new Graph();
+
+            IUriNode dotNetRDF = g.CreateUriNode(UriFactory.Create("http://www.dotnetrdf.org"));
+            IUriNode says = g.CreateUriNode(UriFactory.Create("http://example.org/says"));
+            ILiteralNode helloWorld = g.CreateLiteralNode("Hello World");
+
+            g.Assert(new Triple(dotNetRDF, says, helloWorld));
+
+            sw.Stop();
+            Console.WriteLine("Execution time: " + sw.ElapsedMilliseconds + " ms");
+        }
+
+        private static void PerformanceTestWith2Triples()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            IGraph g = new Graph();
+
+            IUriNode dotNetRDF = g.CreateUriNode(UriFactory.Create("http://www.dotnetrdf.org"));
+            IUriNode says = g.CreateUriNode(UriFactory.Create("http://example.org/says"));
+            ILiteralNode helloWorld = g.CreateLiteralNode("Hello World");
+            ILiteralNode bonjourMonde = g.CreateLiteralNode("Bonjour tout le Monde", "fr");
+
+            g.Assert(new Triple(dotNetRDF, says, helloWorld));
+            g.Assert(new Triple(dotNetRDF, says, bonjourMonde));
+
+            sw.Stop();
+            Console.WriteLine("Execution time: " + sw.ElapsedMilliseconds + " ms");
+        }
+
+        private static void PerformanceTestWith10Triples()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            IGraph g = new Graph();
+
+            IUriNode dotNetRDF = g.CreateUriNode(UriFactory.Create("http://www.dotnetrdf.org"));
+            IUriNode says = g.CreateUriNode(UriFactory.Create("http://example.org/says"));
+            ILiteralNode helloWorld = g.CreateLiteralNode("Hello World");
+            ILiteralNode bonjourMonde = g.CreateLiteralNode("Bonjour tout le Monde", "fr");
+
+            g.Assert(new Triple(dotNetRDF, says, helloWorld));
+            g.Assert(new Triple(dotNetRDF, says, bonjourMonde));
+            g.Assert(new Triple(dotNetRDF, says, helloWorld));
+            g.Assert(new Triple(dotNetRDF, says, bonjourMonde));
+            g.Assert(new Triple(dotNetRDF, says, helloWorld));
+            g.Assert(new Triple(dotNetRDF, says, bonjourMonde));
+            g.Assert(new Triple(dotNetRDF, says, helloWorld));
+            g.Assert(new Triple(dotNetRDF, says, bonjourMonde));
+            g.Assert(new Triple(dotNetRDF, says, helloWorld));
+            g.Assert(new Triple(dotNetRDF, says, bonjourMonde));
+
+            sw.Stop();
+            Console.WriteLine("Execution time: " + sw.ElapsedMilliseconds + " ms");
+        }
+
+        private static void PerformanceTestWith100Triples()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            IGraph g = new Graph();
+
+            IUriNode dotNetRDF = g.CreateUriNode(UriFactory.Create("http://www.dotnetrdf.org"));
+            IUriNode says = g.CreateUriNode(UriFactory.Create("http://example.org/says"));
+            ILiteralNode helloWorld = g.CreateLiteralNode("Hello World");
+
+            for (int i = 0; i < 100; i++)
             {
-                Console.WriteLine(t.ToString());
-                i++;
-                if (i == 100)
-                    break;
+                g.Assert(new Triple(dotNetRDF, says, helloWorld));
             }
+
+            sw.Stop();
+            Console.WriteLine("Execution time: " + sw.ElapsedMilliseconds + " ms");
+        }
+
+        private static void PerformanceTestWith1000Triples()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            IGraph g = new Graph();
+
+            IUriNode dotNetRDF = g.CreateUriNode(UriFactory.Create("http://www.dotnetrdf.org"));
+            IUriNode says = g.CreateUriNode(UriFactory.Create("http://example.org/says"));
+
+            for (int i = 0; i < 100000; i++)
+            {
+                g.Assert(new Triple(dotNetRDF, says, g.CreateLiteralNode("Hello World"+i)));
+            }
+
+            sw.Stop();
+            Console.WriteLine("Execution time: " + sw.ElapsedMilliseconds + " ms");
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("PerformanceTestCreate1Triple");
+            //PerformanceTestWith1Triple();
+
+            //Console.WriteLine("PerformanceTestCreate2Triples");
+            //PerformanceTestWith2Triples();
+
+            //Console.WriteLine("PerformanceTestCreate10Triples");
+            //PerformanceTestWith10Triples();
+
+            //Console.WriteLine("PerformanceTestCreate100Triples");
+            //PerformanceTestWith100Triples();
+
+            Console.WriteLine("PerformanceTestCreate1000Triples");
+            PerformanceTestWith1000Triples();
+
+            //Console.WriteLine("PerformanceTestLoadDatabase:");
+            //PerformanceTestWithDatabase();
 
             Console.ReadKey();
         }
